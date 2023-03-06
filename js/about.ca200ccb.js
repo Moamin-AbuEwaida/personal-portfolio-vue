866cc8d5086f3d0d2f2863da9001b8bba7d03c28
Moamin <moamin.salamah@gmail.com>
2023-03-06 10:58:41 +0100

    adding dist folder

---
+++ js/about.ca200ccb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpersonal_portfolio_vue"]=self["webpackChunkpersonal_portfolio_vue"]||[]).push([[443],{7750:function(n,t,u){u.r(t),u.d(t,{default:function(){return a}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1001),r={},o=(0,l.Z)(r,e,s,!1,null,null,null),a=o.exports}}]);
+//# sourceMappingURL=about.ca200ccb.js.map
